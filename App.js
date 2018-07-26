ace35643b94efa0d9e43f0d1df4589561ee9e377
Bognar Junior <bognar_junior@yahoo.com.br>
2018-07-25 23:27:38 -0300

    Commit layout
    Commit de layout previo da página da revista

--- App.js
+++ App.js
@@ -5,7 +5,24 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <View style={{ flex: 1, flexDirection: 'row' }}>
+          <View style={{ flex: 1, backgroundColor: '#832a0d'}} />
+          <View style={{ flex: 1, backgroundColor: '#f4a460'}} />
+          <View style={{ flex: 1, backgroundColor: '#bc8f8f'}} />
+          <View style={{ flex: 1, backgroundColor: '#cd853f'}} />
+        </View>
+        <View style={{ flex: 1 }}>
         
+        </View>
+        <View style={{ flex: 6 }}>
+          
+        </View>
+        <View style={{ flex: 7 }}>
+          
+        </View>
+        <View style={{ flex: 1 }}>
+          
+        </View>
       </View>
     );
   }
@@ -17,5 +34,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#c19a6b',
+    padding: 13,
   },
 });
