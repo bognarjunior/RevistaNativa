1bd3922184db5aa81fb98f98102aed4f5bf7dd55
Bognar Junior <bognar_junior@yahoo.com.br>
2018-07-31 23:26:37 -0300

    Commit artigo 1
    Commit ajustes no layout do artigo 1

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default class App extends Component {
   render() {
     const mainText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultricies maximus sollicitudin. Integer egestas urna sit amet lorem imperdiet porta. Praesent eget condimentum arcu. Sed at elementum enim, quis facilisis metus.';
+
+    const textArtigo1 = 'Aenean eget rhoncus erat, quis dapibus turpis. Maecenas a iaculis diam, ut ultricies urna. Sed quis lacinia sapien. Duis eleifend diam at cursus convallis. Ut nec dignissim neque. Aliquam erat volutpat. Ut eu leo id nunc feugiat porta sit amet in diam. Integer eleifend, turpis et lacinia pulvinar, arcu nisl porta tellus, feugiat faucibus urna nisl eget orci.';
+
     return (
       <View style={styles.container}>
         <View style={{ flex: 1, flexDirection: 'row' }}>
@@ -30,7 +33,29 @@ export default class App extends Component {
           backgroundColor: '#f8ecc2',
           borderBottomWidth: 6, 
           borderTopWidth: 6 }}>
-          <View style={{flex: 1, borderRightWidth: 3}}>
+          <View style={{ flex: 1, borderRightWidth: 3 }}>
+            <View style={{ flex: 1 }}>
+              <Text style={{ fontSize: 22 }}>Title 1</Text>
+            </View>
+            <View style={{ flex: 6, padding: 8 }}>
+              <View style={{ flex: 1, flexDirection: 'row' }}>
+                <Image 
+                  style={{ flex: 1, width: undefined, height: undefined }} 
+                  source={require('./image/leopard.png')} 
+                />
+                <View style={{ flex: 1, paddingLeft: 8}}>
+                  <Text>
+                    {textArtigo1.slice(0, 40)}
+                  </Text>
+                </View>
+              </View>
+              <View style={{ flex: 1 }}>
+                <Text>
+                {textArtigo1.slice(40, 140)}
+                {textArtigo1.length > 140 ? '...' : '' }
+                </Text>
+              </View>
+            </View>
           </View>
           <View style={{ flex: 1, borderLeftWidth: 3 }}>
           </View>
